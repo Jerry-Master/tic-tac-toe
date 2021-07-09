fd317907bc0103e674e6a44e1f0c24dbfd890379
Jose Pérez Cano <joseperez2000@hotmail.es>
2021-07-09 14:37:20 +0200

    tic-tac-toe beta working

--- js/Piece.js
+++ js/Piece.js
@@ -1,84 +1,75 @@
-
 /***** Tic Tac Toe *****/
+
 class Piece{
     constructor(player, position, vel){
-        this.offset = 50;
-        this.width = 40;
-        this.height = 40;
-        this.valid_positions = [
-        [
-            createVector(this.offset - this.width / 2, this.offset - this.height / 2),
-            createVector((width - 2 * this.offset) / 2 + this.offset - this.width / 2,
-                        this.offset - this.height / 2),
-            createVector((width - 2 * this.offset) + this.offset - this.width / 2,
-                        this.offset - this.height / 2),
-        ],
-        [
-            createVector(this.offset - this.width / 2, 
-                        (height - 2 * this.offset) / 2 + this.offset-  this.height / 2),
-            createVector((width - 2 * this.offset) / 2 + this.offset - this.width / 2,
-                        (height - 2 * this.offset) / 2 + this.offset - this.height / 2),
-            createVector((width - 2 * this.offset) + this.offset - this.width / 2,
-                        (height - 2 * this.offset) / 2 + this.offset - this.height / 2),
-        ],
-        [
-            createVector(this.offset - this.width / 2, 
-                        (height - 2 * this.offset) + this.offset - this.height / 2),
-            createVector((width - 2 * this.offset) / 2 + this.offset - this.width / 2,
-                        (height - 2 * this.offset) + this.offset - this.height / 2),
-            createVector((width - 2 * this.offset) + this.offset - this.width / 2,
-                        (height - 2 * this.offset) + this.offset - this.height / 2),
-        ]
-        ];
-    
         this.player = player;
         this.position = position;
-        this.coordinates = this.valid_positions[position.x][position.y];
+        this.coordinates = createVector(VALID_POSITIONS[position.x][position.y][0],
+                                        VALID_POSITIONS[position.x][position.y][1]);
         this.newPos = null;
         this.vel = vel;
+        this.has_piece = false; // For board only
+        this.num_piece = -1; // For board only
+        this.piece_player = -1; // For board only
     }
 
     update = function(){
       if (!(this.newPos === null)){
-          console.log('in');
-          console.log(this.newPos);
-          console.log(this.valid_positions);
-        let newPosCoords = this.valid_positions[this.newPos.x][this.newPos.y];
-        console.log(newPosCoords);
-        console.log(this.valid_positions);
-        if (!newPosCoords.equals(this.coordinates.x, this.coordinates.y)){
-            this.vel = p5.Vector.sub(newPosCoords, this.coordinates);
-            this.vel.setMag(2);
-            
+        let newPosCoords = createVector(VALID_POSITIONS[this.newPos.x][this.newPos.y][0],
+                                        VALID_POSITIONS[this.newPos.x][this.newPos.y][1]);
+        this.vel = p5.Vector.sub(newPosCoords, this.coordinates);                                        
+        if (this.vel.mag() > 5){
+            this.vel.setMag(5); 
+            this.coordinates.add(this.vel);  
         } else {
             this.vel = createVector(0, 0);
             this.position = this.newPos;
+            this.coordinates = createVector(VALID_POSITIONS[this.newPos.x][this.newPos.y][0],
+                                            VALID_POSITIONS[this.newPos.x][this.newPos.y][1]);
             this.newPos = null;
         }
-        this.coordinates.add(this.vel);
       }
     }
 
     inside = function(x, y){
         let x_diff = (x - this.coordinates.x);
         let y_diff = (y - this.coordinates.y);
-        return (x_diff < this.width) && (y_diff < this.height)
+        return (x_diff < WIDTH) && (y_diff < HEIGHT)
             && (0 < x_diff) && (0 < y_diff);
     }
 
     clicked = function(){
         if (this.inside(mouseX, mouseY)){
-            return this.position;
-        } else {
-            return null;
-        }
+            if (this.has_piece){
+                return this.has_piece;
+            } else {
+                this.has_piece = true;
+                return false;
+            }
+        } 
+        return null;
     }
   
     show = function(){
       push();
-      noStroke();
-      fill(30, 120, 0);
-      rect(this.coordinates.x, this.coordinates.y, this.width, this.height);
+      if (this.player == 0){
+        strokeWeight(5);
+        stroke(0);
+        line(this.coordinates.x, this.coordinates.y, 
+             this.coordinates.x + WIDTH, this.coordinates.y + HEIGHT);
+        line(this.coordinates.x, this.coordinates.y + HEIGHT, 
+             this.coordinates.x + WIDTH, this.coordinates.y,);
+      } else if (this.player == 1){
+        strokeWeight(5);
+        stroke(0);
+        noFill();
+        ellipse(this.coordinates.x + WIDTH / 2, this.coordinates.y + HEIGHT / 2,
+                WIDTH, HEIGHT);
+      } else {
+        noStroke();
+        fill(30, 120, 0);
+        rect(this.coordinates.x, this.coordinates.y, WIDTH, HEIGHT);
+      }
       pop();
     }
   }
--- js/sketch.js
+++ js/sketch.js
@@ -1,12 +1,51 @@
 var board = [];
-var pieces = [];
+var player0 = [];
+var player1 = [];
+var currentPlayer = 0;
+var finished = false;
+var total_pieces = 0;
+var moving_piece = -1;
+var curr_board = -1;
+var waiting_response = false;
+const OFFSET = 80;
+const WIDTH = 80;
+const HEIGHT = 80;
+const SCREEN_WIDHT = 600;
+const SCREEN_HEIGHT = 600;
+const VALID_POSITIONS = [
+    [
+        [OFFSET - WIDTH / 2, OFFSET - HEIGHT / 2],
+        [(SCREEN_WIDHT - 2 * OFFSET) / 2 + OFFSET - WIDTH / 2,
+                    OFFSET - HEIGHT / 2],
+        [(SCREEN_WIDHT - 2 * OFFSET) + OFFSET - WIDTH / 2,
+                    OFFSET - HEIGHT / 2],
+    ],
+    [
+        [OFFSET - WIDTH / 2, 
+                    (SCREEN_HEIGHT - 2 * OFFSET) / 2 + OFFSET-  HEIGHT / 2],
+        [(SCREEN_WIDHT - 2 * OFFSET) / 2 + OFFSET - WIDTH / 2,
+                    (SCREEN_HEIGHT - 2 * OFFSET) / 2 + OFFSET - HEIGHT / 2],
+        [(SCREEN_WIDHT - 2 * OFFSET) + OFFSET - WIDTH / 2,
+                    (SCREEN_HEIGHT - 2 * OFFSET) / 2 + OFFSET - HEIGHT / 2],
+    ],
+    [
+        [OFFSET - WIDTH / 2, 
+                    (SCREEN_HEIGHT - 2 * OFFSET) + OFFSET - HEIGHT / 2],
+        [(SCREEN_WIDHT - 2 * OFFSET) / 2 + OFFSET - WIDTH / 2,
+                    (SCREEN_HEIGHT - 2 * OFFSET) + OFFSET - HEIGHT / 2],
+        [(SCREEN_WIDHT - 2 * OFFSET) + OFFSET - WIDTH / 2,
+                    (SCREEN_HEIGHT - 2 * OFFSET) + OFFSET - HEIGHT / 2],
+    ]
+];
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(SCREEN_WIDHT, SCREEN_HEIGHT);
   for (let i = 0; i < 9; i++){
-    board[i] = new Piece(0, createVector(Math.floor(i / 3), i % 3), createVector(0, 0));
-    pieces[i] = new Piece(0, createVector(Math.floor(i / 3), i % 3), createVector(0, 0));
+    board[i] = new Piece(-1, createVector(Math.floor(i / 3), i % 3), createVector(0, 0));
   }
+  button = createButton('Restart');
+  button.position(SCREEN_WIDHT / 2 - button.width / 2, 0);
+  button.mousePressed(restart);
 }
 
 function draw() {
@@ -14,16 +53,132 @@ function draw() {
   for (let i = 0; i < 9; i++){
     board[i].show();
   }
+  for (let i = 0; i < player0.length; i++){
+    player0[i].update();
+    player0[i].show();
+  }
+  for (let i = 0; i < player1.length; i++){
+    player1[i].update();
+    player1[i].show();
+  }
+  finished = checkGame();
+}
 
-  pieces[0].update();
-  pieces[0].show();
+function check(i, j, k, player){
+  let v1, v2, v3;
+  if (player == 0){
+    v1 = player0[i].position;
+    v2 = player0[j].position;
+    v3 = player0[k].position; 
+  } else {
+    v1 = player1[i].position;
+    v2 = player1[j].position;
+    v3 = player1[k].position; 
+  }
+  let diff1 = p5.Vector.sub(v2, v1);
+  diff1.setMag(1);
+  let diff2 = p5.Vector.sub(v3, v1);
+  diff2.setMag(1);
+
+  if (abs(abs(diff1.dot(diff2)) - 1) < 0.01){
+    return true;
+  }
+  return false;
+}
+
+function checkGame(){
+  for (let i = 0; i < player0.length; i++){
+    for (let j = i+1; j < player0.length; j++){
+      for (let k = j+1; k < player0.length; k++){
+        if (check(i,j,k, 0)){
+          console.log('Player0 won.');
+          return true;
+  } } } }
+  for (let i = 0; i < player1.length; i++){
+    for (let j = i+1; j < player1.length; j++){
+      for (let k = j+1; k < player1.length; k++){
+        if (check(i,j,k, 1)){
+          console.log('Player1 won.');
+          return true;
+  } } } }
+  return false;
 }
 
 function mousePressed(){
-  for (let i = 0; i < 9; i++){
-    newPos = board[i].clicked();
-    if (newPos) {
-      pieces[0].newPos = newPos;
+  console.log(board);
+  if (waiting_response) { // New cell to move the piece
+    let next_board;
+    for (let i = 0; i < 9; i++){
+      next_board = i;
+      let has_piece = board[i].clicked();
+      if (!has_piece && !(has_piece === null) && next_board != curr_board) {
+        if (currentPlayer == 0 && player0.length == 3) {
+          player0[moving_piece].newPos = board[i].position;
+          board[i].num_piece = moving_piece;
+          board[i].piece_player = 0;
+          currentPlayer = 1-currentPlayer;
+        } else if (currentPlayer == 1 && player1.length == 3) {
+          player1[moving_piece].newPos = board[i].position;
+          board[i].num_piece = moving_piece;
+          board[i].piece_player = 1;
+          currentPlayer = 1-currentPlayer;
+        }
+      } else if (has_piece) { // Wrong move
+        board[curr_board].has_piece = true;
+      }
     }
+    waiting_response = false;
+  } else if (!finished){
+    for (let i = 0; i < 9; i++){
+      let has_piece = board[i].clicked();
+      if (!has_piece && !(has_piece === null)) {
+        // Add piece
+        if (currentPlayer == 0 && player0.length < 3) {
+          board[i].num_piece = player0.length;
+          board[i].piece_player = 0;
+          player0.push(new Piece(currentPlayer, 
+                              board[i].position, createVector(0,0)));  
+          currentPlayer = 1-currentPlayer;                            
+        } else if (currentPlayer == 1 && player1.length < 3) {
+          board[i].num_piece = player1.length;
+          board[i].piece_player = 1;
+          player1.push(new Piece(currentPlayer, 
+                      board[i].position, createVector(0,0)));
+          currentPlayer = 1-currentPlayer;
+        } else { // Wrong move
+          board[i].has_piece = false;
+        }
+      } else if (has_piece && board[i].piece_player == currentPlayer){
+        // Move piece
+        if (currentPlayer == 0 && player0.length == 3) {
+          board[i].has_piece = false;
+          moving_piece = board[i].num_piece;
+          curr_board = i;
+          waiting_response = true;
+        } else if (currentPlayer == 1 && player1.length == 3) {
+          board[i].has_piece = false;
+          moving_piece = board[i].num_piece;
+          curr_board = i;
+          waiting_response = true;
+        }
+      }
+    }
+  } 
+  finished = checkGame();
+  console.log(waiting_response);
+  console.log(currentPlayer);
+}
+
+function restart(){
+  player0 = [];
+  player1 = [];
+  currentPlayer = 0;
+  finished = false;
+  total_pieces = 0;
+  moving_piece = -1;
+  curr_board = -1;
+  waiting_response = false;
+  for (let i = 0; i < 9; i++){
+    board[i].has_piece = false;
   }
 }
